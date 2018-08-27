9c06cbddc09a9d45786d3c54a770253355e0661e
minirusina <42751935+minirusina@users.noreply.github.com>
2018-08-27 23:09:00 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
 # world-boss-helper
+A `tera-proxy` module that aims to help with searching for and keeping track of World Bosses.
+
+## Features
+- Notifies you in the middle of the screen, if a World Boss is found / is dead / moves out of range
+- Spawns a Vergos' Head under the boss for more visibility
+- Uploads kill times to a spreadsheet, that is publicly available [HERE](https://tera.zone/worldboss/eu/)
+- It can send notifications to a Discord webhook, so that your friends/guild mates can instantly know you found a boss
+- Supports Caali's auto update
+
+## Dependencies
+- `command`
+- `tera-vec3`
+- `request`
+
+If you use Pinkie's proxy, then you need to run `npm install request` in the main proxy directory. If you use Caali's proxy, then you're good to go, because `request` is already bundled in it.
+
+## Config
+You can edit `config.json` to change default settings. If you want to notify your friends/guild on Discord, you can create a Webhook, and enter the URL in the config.
+
+## Usage
+### `wbh`
+- Toggle on/off
+- Default is on
+### `wbh alert`
+- Toggle popup notice on/off
+- Default is on
+### `wbh msg`
+- Toggle system message on/off
+- Default is on
+### `wbh mark`
+- Toggle markers on/off
+- Default is on
+### `wbh clear`
+- Attempts to clear markers
+### `wbh ui`
+- Shows timers via in game browser
+
+## Changelog
+### 2018-06-01
+- Updated to `dispatch.send()`
+- Fixed incompatibility with `no-more-death-animations`
+### 2018-05-28
+- Character names will only be uploaded, if Discord notifications are enabled
+- Removed warn list (this was a useful feature back when everyone was hunting/stealing bosses, but probably not needed anymore)
+
+## Credits
+- This module was originally based on an earlier version of [WarnMe](https://github.com/SerenTera/WarnMe) by [SerenTera](https://github.com/SerenTera)
---
+++ index.js
@@ -0,0 +1,206 @@
+const Command = require('command')
+const Vec3 = require('tera-vec3')
+const request = require('request')
+const config = require('./config.json')
+const bosses = require('./bosses.json')
+const itemId = 98260
+
+let enabled = config.enabled
+let alerted = config.alerted
+let messager = config.messager
+let marker = config.marker
+let discord = config.discordWebhookUrl
+let mention = config.mention
+
+module.exports = function WorldBossHelper(dispatch) {
+  const command = Command(dispatch)
+
+  let playerName
+  let currentZone
+  let bossName
+  let mobid = []
+
+  command.add('wbh', (arg1, arg2) => {
+    switch (arg1) {
+      case 'alert':
+        alerted = !alerted
+        command.message(alerted ? ' (World-Boss) System popup notice: [' + green('ON') + '].' : ' (World-Boss) System popup notice: [' + red('OFF') + '].')
+        break
+
+      case 'msg':
+        messager = !messager
+        command.message(messager ? ' (World-Boss) System message: [' + green('ON') + '].' : ' (World-Boss) System message: [' + red('OFF') + '].')
+        break
+
+      case 'mark':
+        marker = !marker
+        command.message(marker ? ' (World-Boss) Markers: [' + green('ON') + '].' : ' (World-Boss) Markers: [' + red('OFF') + '].')
+        break
+
+      case 'clear':
+        command.message(' (World-Boss) Markers cleared.')
+        for (let itemId of mobid) {
+          despawnItem(itemId)
+        }
+        break
+
+      case 'ui':
+        dispatch.send('S_OPEN_AWESOMIUM_WEB_URL', 1, {
+          url: 'tera.zone/worldboss/ingame.php?serverId=' + serverId
+        })
+        break
+
+      default:
+        enabled = !enabled
+        command.message(enabled ? ' (World-Boss) Module: [' + green('ON') + '].' : ' (World-Boss) Module: [' + red('OFF') + '].')
+        if (!enabled) {
+          for (let itemId of mobid) {
+            despawnItem(itemId)
+          }
+        }
+    }
+  })
+
+  dispatch.hook('S_LOGIN', 10, (event) => {
+    serverId = event.serverId
+    if (discord) {
+      playerName = event.name
+    } else {
+      playerName = "Anonymous"
+    }
+  })
+
+  dispatch.hook('S_LOAD_TOPO', 3, (event) => {
+    currentZone = event.zone
+    mobid = []
+  })
+
+  dispatch.hook('S_CURRENT_CHANNEL', 2, (event) => {
+    currentChannel = event.channel
+  })
+
+  dispatch.hook('S_SPAWN_NPC', 8, (event) => {
+    let boss
+    if (enabled && (boss = bosses.filter(b => b.huntingZoneId.includes(event.huntingZoneId) && b.templateId === event.templateId)[0])) {
+      bossName = boss.name
+      if (marker) {
+        spawnItem(event.loc, event.gameId.low)
+        mobid.push(event.gameId.low)
+      }
+      request.post('https://i.imgur.com/CHd83ga.png', {
+        form: {
+          serverId: serverId,
+          playerName: playerName,
+          bossName: bossName,
+          channel: currentChannel,
+          time: new Date().getTime(),
+          status: 'found',
+          discord: encodeURIComponent(discord),
+          mention: mention
+        }
+      }, function(err, httpResponse, body) {
+        if (err) {
+          console.log(err)
+        } else {
+          console.log('[world-boss] ' + body)
+        }
+      })
+      if (alerted) {
+        notice('Found boss: ' + bossName + '!')
+      }
+      if (messager) {
+        command.message(' (World-Boss) Found boss: ' + bossName + '!')
+      }
+    }
+  })
+
+  dispatch.hook('S_DESPAWN_NPC', 3, {order: -100}, (event) => {
+    if (mobid.includes(event.gameId.low)) {
+      if (alerted && bossName) {
+        if (event.type == 5) {
+          request.post('https://i.imgur.com/CHd83ga.png', {
+            form: {
+              serverId: serverId,
+              playerName: playerName,
+              bossName: bossName,
+              channel: currentChannel,
+              time: new Date().getTime(),
+              status: 'killed',
+              discord: encodeURIComponent(discord)
+            }
+          }, function(err, httpResponse, body) {
+            if (err) {
+              console.log(err)
+            } else {
+              console.log('[world-boss] ' + body)
+            }
+          })
+          if (alerted) {
+            notice(bossName + ' is dead!')
+          }
+          if (messager) {
+            command.message(' (World-Boss) ' + bossName + ' is dead!')
+          }
+        } else if (event.type == 1) {
+          if (alerted) {
+            notice(bossName + ' is out of range...')
+          }
+          if (messager) {
+            command.message(' (World-Boss) ' + bossName + ' is out of range...')
+          }
+        }
+      }
+      bossName = null
+      despawnItem(event.gameId.low)
+      mobid.splice(mobid.indexOf(event.gameId.low), 1)
+    }
+  })
+
+  function spawnItem(loc, gameId) {
+    dispatch.send('S_SPAWN_DROPITEM', 6, {
+      gameId: {
+        low: gameId,
+        high: 0,
+        unsigned: true
+      },
+      loc: loc,
+      item: itemId,
+      amount: 1,
+      expiry: 600000,
+      owners: [{
+        id: 0
+      }]
+    })
+  }
+
+  function despawnItem(gameId) {
+    dispatch.send('S_DESPAWN_DROPITEM', 4, {
+      gameId: {
+        low: gameId,
+        high: 0,
+        unsigned: true
+      }
+    })
+  }
+
+  function notice(msg) {
+    dispatch.send('S_DUNGEON_EVENT_MESSAGE', 2, {
+      type: 42,
+      chat: 0,
+      channel: 0,
+      message: msg
+    })
+  }
+
+  function green(text) {
+    return '<font color="#78DD56">' + text + '</font>'
+  }
+
+  function red(text) {
+    return '<font color="#FF7F7F">' + text + '</font>'
+  }
+
+  this.destructor = function() {
+    command.remove('wbh')
+  }
+}
